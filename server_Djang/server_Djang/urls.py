e9b3811c4cd1926297f896fa891622428fe667bf
medvedevmm07 <75927287+eSwerq@users.noreply.github.com>
2022-04-03 17:55:00 +0600

    030422

--- server_Djang/server_Djang/urls.py
+++ server_Djang/server_Djang/urls.py
@@ -4,12 +4,13 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from welcomeApp.views import initPage, aboutPage
-from twoapp.views import buildPage
+from twoapp.views import buildPage, CurrentBuild
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', initPage),
     path('about/', aboutPage, name="about_path" ),
-    path('buildings/', buildPage, name = "buildscan")
+    path('buildings/', buildPage, name = "buildscan"),
+    path('build/', CurrentBuild, name = "current")
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- server_Djang/twoapp/views.py
+++ server_Djang/twoapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from twoapp.models import WoodBuildings
+
 def buildPage(request):
-    return render (request, "buildings.html")
+    allObects = WoodBuildings.objects.all()
+    params = { "build":allObects}
+    return render (request, "buildings.html", params)
+
+def CurrentBuild(request):
+    return "Currnt Build"
